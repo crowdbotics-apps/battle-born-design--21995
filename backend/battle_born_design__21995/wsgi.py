8d279fe69abd86340d9ad3e8b3e3964d43e5973a
Crowdbotics <team@crowdbotics.com>
2020-10-27 03:05:23 +0000

    Update models

--- backend/battle_born_design__21995/wsgi.py
+++ backend/battle_born_design__21995/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'battle_born_design__21995.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "battle_born_design__21995.settings")
 
 application = get_wsgi_application()
